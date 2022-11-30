9a28551ec287c42df95c90cfc5fd9fa513284161
Никита Макарьев <nikita.mackariev@yandex.ru>
2022-12-01 01:08:47 +0300

    Добавлены тесты PEP8

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,11 +9,11 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
-from .serializers import (
-    PasswordSerializer,
-    UserSerializer
-)
+
 from .models import Follow, User
+from .serializers import (
+    UserSerializer,
+    PasswordSerializer)
 
 USER_BLOCKED = 'Аккаунт не активен!'
 
